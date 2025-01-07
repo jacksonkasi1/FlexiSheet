f7b155963a9ee04274029e3d6a1a0f3024033b9a
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2025-01-07 08:27:18 +0000

    feat: implement theme provider and header component with theme toggle functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +31,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
